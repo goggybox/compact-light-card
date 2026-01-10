c29aacc20d5040ef5efba759ad816a12da8bb2e8
lewisburgess <lewisburgess@users.noreply.github.com>
2026-01-10 10:58:44 +0000

    Rename to match original compact-light-card naming

--- compact-light-card.js
+++ compact-light-card.js
@@ -8,7 +8,7 @@
  */
 
 
-console.log("compact-light-card.js v0.6.7 loaded!");
+console.log("compact-light-card.js v0.6.9 loaded!");
 window.left_offset = 66;
 
 class CompactLightCard extends HTMLElement {
